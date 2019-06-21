02fa68ce19654c2417768cb71a1c17e71ab8b472
areevesman <areevesman@gmail.com>
2019-06-21 10:12:11 -0700

    refactor and cleanup

--- code/snake.py
+++ code/snake.py
@@ -17,24 +17,18 @@ class Apple():
 
 class Snake():
 
-    def __init__(self, step=50, direction=0, length=5,
-                 update_count_max=2, update_count=0):
+    def __init__(self, step=50, direction=0, length=5, human=True):
         self.step = step
         self.direction = direction
         self.length = length
-        self.update_count_max = update_count_max
-        self.update_count = update_count
-
-        # initial positions, no collision.
         self.x = [(i + 1) * self.step for i in range(self.length - 1, -1, -1)]
         self.y = [1 * self.step for i in range(self.length - 1, -1, -1)]
+        self.human = human
+        if self.human:
+            self.update_count_max = 2
+            self.update_count = 0
 
-    def update(self):
-        #         print(list(zip(self.x,self.y)))
-        #         print(self.update_count)
-
-        self.update_count = self.update_count + 1
-        if self.update_count > self.update_count_max:
+    def update_snake(self):
 
             # update previous positions
             for i in range(self.length - 1, 0, -1):
@@ -51,7 +45,15 @@ class Snake():
             if self.direction == 3:
                 self.y[0] = self.y[0] + self.step
 
-            self.update_count = 0
+    def update(self):
+
+        if self.human:
+            self.update_count = self.update_count + 1
+            if self.update_count > self.update_count_max:
+                self.update_snake()
+                self.update_count = 0
+        else:
+            self.update_snake()
 
     def moveRight(self):
         self.direction = 0
@@ -73,13 +75,9 @@ class Snake():
 class Game():
 
     def __init__(self, window_width=500, window_height=500, player=Snake(), apple=Apple()):
-        self._running = True
-        self._display_surf = None
-        self._image_surf = None
-        self._apple_surf = None
         self.window_width = window_width
         self.window_height = window_height
-        self.player = player
+        self.snake = player
         self.apple = apple
 
     def is_collision(self, x1, y1, x2, y2):
@@ -87,69 +85,71 @@ class Game():
             return True
         return False
 
-    def out_of_bounds(self, x1, y1):
+    def is_out_of_bounds(self, x1, y1):
         if ((x1 < 0) or (y1 < 0) or
-                (x1 > self.window_width-self.player.step) or (y1 > self.window_height-self.player.step)):
+                (x1 > self.window_width-self.snake.step) or (y1 > self.window_height - self.snake.step)):
             return True
         return False
 
-    def on_loop(self):
-        self.player.update()
-        # print(list(zip(self.player.x, self.player.y)))
+    def place_apple(self):
+        # place apple
+        self.apple = Apple(randint(0, self.window_width / self.snake.step - 1),
+                           randint(0, self.window_height / self.snake.step - 1))
+        # check for collision with snake
+        while (self.apple.x, self.apple.y) in list(zip(self.snake.x, self.snake.y)):
+            self.apple = Apple(randint(0, self.window_width / self.snake.step - 1),
+                               randint(0, self.window_height / self.snake.step - 1))
 
-        # does snake eat apple?
-        if self.is_collision(self.apple.x, self.apple.y,
-                             self.player.x[0], self.player.y[0]):
-            self.player.x = self.player.x + [self.player.x[-1]]
-            self.player.y = self.player.y + [self.player.y[-1]]
-            print(list(zip(self.player.x, self.player.y)))
-            self.player.length = self.player.length + 1
 
-            self.apple = Apple(randint(0, self.window_width / self.player.step - 1),
-                               randint(0, self.window_height / self.player.step - 1))
-            while (self.apple.x, self.apple.y) in list(zip(self.player.x, self.player.y)):
-                self.apple = Apple(randint(0, self.window_width / self.player.step - 1),
-                                   randint(0, self.window_height / self.player.step - 1))
+    def try_eat_apple(self):
+        # if snake finds apple
+        if self.is_collision(self.apple.x,
+                             self.apple.y,
+                             self.snake.x[0],
+                             self.snake.y[0]):
+            # update snake
+            self.snake.x = self.snake.x + [self.snake.x[-1]]
+            self.snake.y = self.snake.y + [self.snake.y[-1]]
+            self.snake.length = self.snake.length + 1
+            print(f'Score: {self.snake.length}')
 
+            self.place_apple()
+
+
+    def try_hit_self(self):
         # does snake collide with itself?
-        for i in range(2, self.player.length - 1):
-            #             print(i,list(zip(self.player.x,self.player.y)))
-            hit_self = self.is_collision(self.player.x[0],
-                                         self.player.y[0],
-                                         self.player.x[i],
-                                         self.player.y[i])
+        for i in range(2, self.snake.length - 1):
+            if self.is_collision(self.snake.x[0],
+                                 self.snake.y[0],
+                                 self.snake.x[i],
+                                 self.snake.y[i]):
 
-            if hit_self:
-                print(self.player.x[0], self.player.y[0])
-                print(self.player.x[i], self.player.y[i])
+                print(self.snake.x[0], self.snake.y[0])
+                print(self.snake.x[i], self.snake.y[i])
 
                 print("You lose! Hit yourself: ")
-                print("x[0] (" + str(self.player.x[0]) + "," + str(self.player.y[0]) + ")")
-                print("x[" + str(i) + "] (" + str(self.player.x[i]) + "," + \
-                      str(self.player.y[i]) + ")")
-                #                 print('hit yourself')
-                self.player = Snake()
+                print("x[0] (" + str(self.snake.x[0]) + "," + str(self.snake.y[0]) + ")")
+                print("x[" + str(i) + "] (" + str(self.snake.x[i]) + "," + \
+                      str(self.snake.y[i]) + ")")
 
-                self.apple = Apple(randint(0, self.window_width / self.player.step - 1),
-                                   randint(0, self.window_height / self.player.step - 1))
-
+                # should change this to a random snake position in the future
+                self.snake = Snake()
+                self.place_apple()
                 break
 
-        if self.out_of_bounds(self.player.x[0], self.player.y[0]):
+    def try_out_of_bounds(self):
+        if self.is_out_of_bounds(self.snake.x[0], self.snake.y[0]):
             print("You lose! Hit wall: ")
-            print("x[0] (" + str(self.player.x[0]) + "," + str(self.player.y[0]) + ")")
-            print("x[" + str(i) + "] (" + str(self.player.x[i]) + "," + \
-                  str(self.player.y[i]) + ")")
-            print('hit wall')
-            self.player = Snake()
-
-            self.apple = Apple(randint(0, self.window_width / self.player.step - 1),
-                               randint(0, self.window_height / self.player.step - 1))
-            while (self.apple.x, self.apple.y) in list(zip(self.player.x, self.player.y)):
-                self.apple = Apple(randint(0, self.window_width / self.player.step - 1),
-                                   randint(0, self.window_height / self.player.step - 1))
-
+            print("x[0] (" + str(self.snake.x[0]) + "," + str(self.snake.y[0]) + ")")
+            self.snake = Snake()
+            self.place_apple()
         pass
+
+    def make_move(self):
+        self.snake.update()
+        self.try_eat_apple()
+        self.try_hit_self()
+        self.try_out_of_bounds()
 
 
 class App():
@@ -167,10 +167,10 @@ class App():
                                                       self.game.window_height),
                                                      pygame.HWSURFACE)
 
-        pygame.display.set_caption('Pygame pythonspot.com example')
+        pygame.display.set_caption('Time to learn!')
         self._running = True
-        self._image_surf = pygame.image.load("./images/snake1.png").convert()
-        self._apple_surf = pygame.image.load("./images/apple_img.png").convert()
+        self._image_surf = pygame.image.load("../images/snake2.png").convert()
+        self._apple_surf = pygame.image.load("../images/apple_img.png").convert()
 
     def on_event(self, event):
         if event.type == QUIT:
@@ -178,7 +178,7 @@ class App():
 
     def on_render(self):
         self._display_surf.fill((0, 0, 0))
-        self.game.player.draw(self._display_surf, self._image_surf)
+        self.game.snake.draw(self._display_surf, self._image_surf)
         self.game.apple.draw(self._display_surf, self._apple_surf)
         pygame.display.flip()
 
@@ -197,36 +197,36 @@ class App():
 
             if (keys[K_RIGHT]):
             # if action == 0:
-                if self.game.player.direction == 1:
-                    self.game.player.moveLeft()
+                if self.game.snake.direction == 1:
+                    self.game.snake.moveLeft()
                 else:
-                    self.game.player.moveRight()
+                    self.game.snake.moveRight()
 
             if (keys[K_LEFT]):
             # if action == 1:
-                if self.game.player.direction == 0:
-                    self.game.player.moveRight()
+                if self.game.snake.direction == 0:
+                    self.game.snake.moveRight()
                 else:
-                    self.game.player.moveLeft()
+                    self.game.snake.moveLeft()
 
             if (keys[K_UP]):
             # if action == 2:
-                if self.game.player.direction == 3:
-                    self.game.player.moveDown()
+                if self.game.snake.direction == 3:
+                    self.game.snake.moveDown()
                 else:
-                    self.game.player.moveUp()
+                    self.game.snake.moveUp()
 
             if (keys[K_DOWN]):
             # if action == 3:
-                if self.game.player.direction == 2:
-                    self.game.player.moveUp()
+                if self.game.snake.direction == 2:
+                    self.game.snake.moveUp()
                 else:
-                    self.game.player.moveDown()
+                    self.game.snake.moveDown()
 
             # if (keys[K_ESCAPE]):
             #     self._running = False
 
-            self.game.on_loop()
+            self.game.make_move()
             self.on_render()
 
             time.sleep(50.0 / 1000.0);
