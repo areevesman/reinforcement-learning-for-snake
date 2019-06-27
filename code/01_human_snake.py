7d955b6fa65ac2fd7a0f66dad8fc518a0d6a731b
areevesman <areevesman@gmail.com>
2019-06-27 07:06:48 -0700

    update human version

--- code/01_human_snake.py
+++ code/01_human_snake.py
@@ -5,7 +5,6 @@ import time
 
 
 class Apple():
-
     def __init__(self, x=5, y=5, step=50):
         self.step = step
         self.x = x * self.step
@@ -16,44 +15,36 @@ class Apple():
 
 
 class Snake():
-
-    def __init__(self, step=50, direction=0, length=5, human=True):
+    def __init__(self, step=50, direction=0, length=5):
         self.step = step
         self.direction = direction
         self.length = length
         self.x = [(i + 1) * self.step for i in range(self.length - 1, -1, -1)]
         self.y = [1 * self.step for i in range(self.length - 1, -1, -1)]
-        self.human = human
-        if self.human:
-            self.update_count_max = 2
-            self.update_count = 0
+        self.update_count_max = 2
+        self.update_count = 0
 
     def update_snake(self):
+        # update previous positions
+        for i in range(self.length - 1, 0, -1):
+            self.x[i] = self.x[i - 1]
+            self.y[i] = self.y[i - 1]
 
-            # update previous positions
-            for i in range(self.length - 1, 0, -1):
-                self.x[i] = self.x[i - 1]
-                self.y[i] = self.y[i - 1]
-
-            # update position of head of snake
-            if self.direction == 0:
-                self.x[0] = self.x[0] + self.step
-            if self.direction == 1:
-                self.x[0] = self.x[0] - self.step
-            if self.direction == 2:
-                self.y[0] = self.y[0] - self.step
-            if self.direction == 3:
-                self.y[0] = self.y[0] + self.step
+        # update position of head of snake
+        if self.direction == 0:
+            self.x[0] = self.x[0] + self.step
+        if self.direction == 1:
+            self.x[0] = self.x[0] - self.step
+        if self.direction == 2:
+            self.y[0] = self.y[0] - self.step
+        if self.direction == 3:
+            self.y[0] = self.y[0] + self.step
 
     def update(self):
-
-        if self.human:
-            self.update_count = self.update_count + 1
-            if self.update_count > self.update_count_max:
-                self.update_snake()
-                self.update_count = 0
-        else:
+        self.update_count = self.update_count + 1
+        if self.update_count > self.update_count_max:
             self.update_snake()
+            self.update_count = 0
 
     def moveRight(self):
         self.direction = 0
@@ -73,7 +64,6 @@ class Snake():
 
 
 class Game():
-
     def __init__(self, window_width=500, window_height=500, player=Snake(), apple=Apple()):
         self.window_width = window_width
         self.window_height = window_height
@@ -100,7 +90,6 @@ class Game():
             self.apple = Apple(randint(0, self.window_width / self.snake.step - 1),
                                randint(0, self.window_height / self.snake.step - 1))
 
-
     def try_eat_apple(self):
         # if snake finds apple
         if self.is_collision(self.apple.x,
@@ -114,7 +103,6 @@ class Game():
             print(f'Score: {self.snake.length}')
 
             self.place_apple()
-
 
     def try_hit_self(self):
         # does snake collide with itself?
@@ -153,24 +141,24 @@ class Game():
 
 
 class App():
-
-    def __init__(self, game=Game()):
+    def __init__(self, game=Game(), caption='Playing snake'):
         self._running = True
         self._display_surf = None
         self._image_surf = None
         self._apple_surf = None
         self.game = game
+        self.caption = caption
 
     def on_init(self):
         pygame.init()
         self._display_surf = pygame.display.set_mode((self.game.window_width,
-                                                      self.game.window_height),
-                                                     pygame.HWSURFACE)
+                                                    self.game.window_height),
+                                                    pygame.HWSURFACE)
 
         pygame.display.set_caption('Time to learn!')
         self._running = True
         self._image_surf = pygame.image.load("../images/snake2.png").convert()
-        self._apple_surf = pygame.image.load("../images/apple_img.png").convert()
+        self._apple_surf = pygame.image.load("../images/apple.png").convert()
 
     def on_event(self, event):
         if event.type == QUIT:
@@ -196,43 +184,39 @@ class App():
             action = randint(0,3)
 
             if (keys[K_RIGHT]):
-            # if action == 0:
                 if self.game.snake.direction == 1:
                     self.game.snake.moveLeft()
                 else:
                     self.game.snake.moveRight()
 
             if (keys[K_LEFT]):
-            # if action == 1:
                 if self.game.snake.direction == 0:
                     self.game.snake.moveRight()
                 else:
                     self.game.snake.moveLeft()
 
             if (keys[K_UP]):
-            # if action == 2:
                 if self.game.snake.direction == 3:
                     self.game.snake.moveDown()
                 else:
                     self.game.snake.moveUp()
 
             if (keys[K_DOWN]):
-            # if action == 3:
                 if self.game.snake.direction == 2:
                     self.game.snake.moveUp()
                 else:
                     self.game.snake.moveDown()
 
-            # if (keys[K_ESCAPE]):
-            #     self._running = False
+            if (keys[K_ESCAPE]):
+                self._running = False
 
             self.game.make_move()
             self.on_render()
 
-            time.sleep(50.0 / 1000.0);
+            time.sleep(20.0 / 1000.0)
         self.on_cleanup()
 
 
 if __name__ == "__main__":
-    theApp = App()
+    theApp = App(caption='Playing Snake')
     theApp.on_execute()
